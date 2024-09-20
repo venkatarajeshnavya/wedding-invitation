70c81ee426f016659e229d6fa2227f0ef3e8a1a7
manojkumar-bandlamudi <bandlamudimanoj@gmail.com>
2024-09-20 11:58:23 +0930

    Updated timer

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2024,
+                month: 10,
+                day: 16,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
